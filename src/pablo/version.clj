c93523617218799bd31378e378249df345578d72
Ryan Schmukler <rschmukler@gmail.com>
2020-02-10 17:19:36 -0500

    fix: version w/ utils/with-cwd
    Resolves git repo paths in the overridden cwd.

--- src/pablo/version.clj
+++ src/pablo/version.clj
@@ -1,6 +1,8 @@
 (ns pablo.version
   "Namespace for detecting versioning"
-  (:require [clj-jgit.porcelain :as git]))
+  (:require [clj-jgit.porcelain :as git]
+            [clj-jgit.querying :as git.query]
+            [me.raynes.fs :as fs]))
 
 (defn from-tags
   "Pure function to compute the version from a list of tags and whether the repo is dirty.
@@ -18,11 +20,23 @@
 (defn from-git
   "Scans the git repo to determine the version of the project"
   []
-  (git/with-repo "."
+  (git/with-repo (fs/file ".")
     (let [tags   (git/git-tag-list repo)
           dirty? (some? (->> (git/git-status repo)
                              (keep not-empty)))]
       (from-tags tags dirty?))))
 
+(defn last-commit-sha
+  "Return a string of the last commit sha of the project"
+  []
+  (git/with-repo (fs/file ".")
+    (->> (git/git-log repo)
+         first
+         :id
+         (git.query/commit-info repo)
+         :id)))
+
 (comment
-  (from-git))
+  (from-git)
+
+  (last-commit-sha))
